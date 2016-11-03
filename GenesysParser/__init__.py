fa2d2ddd39c6207f839e75186899e3a23107d7bb
Eliana_Papoutsoglou <evangelia.papoutsoglou@wur.nl>
2016-11-03 18:22:16 +0100

    Add folder structure

---
+++ GenesysParser/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+"""Queries Genesys via its API using the specified parameters.
+   Then transforms the JSON response into an object. """
---
+++ GenesysParser/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Program entry point"""
+
+def main():
+    pass
+
+
+
+
+if __name__ == '__main__':
+    main()
